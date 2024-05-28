4ceef6c8419412f012d8f77829c1f4acd03f5ea6
Charles Sieg <crsieg@gmail.com>
2024-05-28 14:56:22 -0500

    Added exception handling.

--- lib/sts.py
+++ lib/sts.py
@@ -11,15 +11,17 @@ class STSAPI(AWSAPI):
   __client_name__ = "sts"
 
   def fetch_credentials(self, role_arn):
-    # Call the assume_role method of the STSConnection object and pass the role
-    # ARN and a role session name.
-    assumed_role_object = self.client.assume_role(
-      RoleArn=role_arn,
-      RoleSessionName="AssumeRoleSession1"
-    )
-
-    # From the response that contains the assumed role, get the temporary
-    # credentials that can be used to make subsequent API calls
-    credentials=assumed_role_object['Credentials']
-
-    return credentials
+    try:
+      # Call the assume_role method of the STSConnection object and pass the role
+      # ARN and a role session name.
+      assumed_role_object = self.client.assume_role(
+        RoleArn=role_arn,
+        RoleSessionName="AssumeRoleSession1"
+      )
+      # From the response that contains the assumed role, get the temporary
+      # credentials that can be used to make subsequent API calls
+      credentials=assumed_role_object['Credentials']
+      return credentials
+    except BaseException as err:
+        log.error(f"Unexpected {err=}, {type(err)=}")
+        return BaseException()
